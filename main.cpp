640f4621723ebf11e1f14bac52d30f7ac97feb97
wassim dhiba <wassimdhibaa@gmail.com>
2022-03-03 17:30:02 +0100

    vector container in progress

--- main.cpp
+++ main.cpp
@@ -1,52 +1,15 @@
 #include "vector.hpp"
+#include "utils.hpp"
 #include <vector>
-#include <typeinfo>
 
 int main()
 {
-    ft::vector<int> test;
-    
-    test.push_back(100);
-    test.push_back(100);
-    test.push_back(100);
-    test.push_back(100);
-    test.push_back(100);
-    test.pop_back();
-    test.pop_back();
-    test.pop_back();
-    test.push_back(20);
-    unsigned int t = 10;
-    ft::vector_iterator<int> it = test.begin(); 
-    ft::vector_iterator<int> ite = test.end();
-    test.assign(it, ite);
-    it = test.begin(); 
-    ite = test.end();
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        it++;
-    }
-    std::cout << "------------------------------------\n";
-    std::vector<int> test1;
-    
-    test1.push_back(100);
-    test1.push_back(100);
-    test1.push_back(100);
-    test1.push_back(100);
-    test1.push_back(100);
-    test1.pop_back();
-    test1.pop_back();
-    test1.pop_back();
-    test1.push_back(20);
-    std::vector<int>::iterator ok = test1.begin();
-    std::vector<int>::iterator oke = test1.end();
-    test1.assign(ok, oke);
-    oke = test1.end();
-    ok = test1.begin();
-    while (ok != oke)
-    {
-        std::cout << "oui " << *ok << std::endl;
-        ok++;
-    }
-    return 0;
+    ft::pair<int, std::string> P1(10, "salut bg");
+    std::cout << P1.second << std::endl;
+    ft::pair<int, std::string> P3;
+    std::cout << P3.first << std::endl;
+
+    std::pair<int, std::string> P2;
+    std::cout << P2.first << std::endl;
+    return 1;
 }
