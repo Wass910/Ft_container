8b7197f5248454d4e73dacef5ff963fbcd848934
wassim dhiba <wassimdhibaa@gmail.com>
2022-03-03 18:04:27 +0100

    vector container in progress

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include "vector.hpp"
-#include "utils.hpp"
+#include "utils/make_pair.hpp"
 #include <vector>
 
 int main()
 {
-    ft::pair<int, std::string> P1(10, "salut bg");
-    std::cout << P1.second << std::endl;
-    ft::pair<int, std::string> P3;
+    ft::pair<int, int> P1(10, 100);
+    ft::pair<int, int> P3;
+    //P1.first++;
+    //P3.second++;
+    P3 = ft::make_pair<int , char>(10, 'c');
     std::cout << P3.first << std::endl;
+    std::cout << P3.second << std::endl;
 
-    std::pair<int, std::string> P2;
-    std::cout << P2.first << std::endl;
+    std::pair<int, int> P2(10, 100);
+    std::pair<int, int> P4;
+    //P2.first++;
+    //P4.second++;
+    P4 = std::make_pair<int , char>(10, 50);
+    std::cout << P4.first << std::endl;
+    std::cout << P4.second << std::endl;
     return 1;
 }
---
+++ utils/pair.hpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <typeinfo>
+
+namespace ft{
+    template <class T1, class T2>
+    class pair{
+        public:
+            pair( void ) : first(), second()
+            {
+                return ;
+            }
+
+            pair (const T1 & a, const T2 & b) : first(a), second(b)
+            {
+                return ;
+            }
+
+            pair & operator=(const pair& pr)
+            {
+                if (pr.first)
+                    this->first = pr.first;
+                if (pr.second)
+                    this->second = pr.second;
+                return *this;
+            }
+
+            template<class U, class V>
+            pair (const pair<U,V>& pr)
+            {
+                *this = pr;
+                return ;
+            }
+            
+            ~pair( void )
+            {
+                std::cout << "Pair destructor is call.\n";
+                return ;
+            }    
+
+            
+            friend bool operator==(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return lhs.first==rhs.first && lhs.second==rhs.second; 
+            }
+            
+            friend bool operator!=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return !(lhs==rhs); 
+            }
+            
+            friend bool operator<(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); 
+            }
+        
+            friend bool operator<=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return !(rhs<lhs); 
+            }
+            
+            friend bool operator>(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return rhs<lhs; 
+            }
+
+            friend bool operator>=(const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+            { 
+                return !(lhs<rhs); 
+            }
+
+            T1 first;
+            T2 second;
+};
+}
