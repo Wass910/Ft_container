35b5edfdfdd0b372585e47ce0223b44582656b5d
wassim dhiba <wassimdhibaa@gmail.com>
2022-03-09 19:06:41 +0100

    map container in progress

--- Map/main.cpp
+++ Map/main.cpp
@@ -4,11 +4,12 @@
 
 int	main()
 {
-    // std::map<std::string, int> trezor;
+     //std::map<std::string, int> trezor;
     // trezor["wassim trop bg"] = 69;
-    // trezor["gildas le plus bg"] = 15;
-    // std::cout << trezor["gildas le plus bg"] << std::endl;
+    //trezor["gildas le plus bg"] = 15;
     ft::map<std::string, int> trezor;
+    trezor["gildas le plus bg"] = 15;
     trezor["wassim trop bg"] = 69;
+    std::cout << trezor["wassim trop bg"] << std::endl;
 	return 0;
 }
--- Map/map.hpp
+++ Map/map.hpp
@@ -6,18 +6,22 @@
 #include <memory>
 #include <exception>
 #include <iterator>
+#include "../utils/make_pair.hpp"
 
 namespace ft
 {
 
-    template < class Key,                          //map::key_type
-	class T,                                       // map::mapped_type
-	class Compare = std::less<Key>,                     // map::key_compare
-	class Alloc = std::allocator<std::pair<const Key,T> > >    // map::allocator_type
+    template < class Key, class T, class Compare = std::less<Key>,  
+    class Alloc = std::allocator<std::pair<const Key,T> > >    
     class map{
         public:
+
+            typedef size_t												    size_type;
+
             map( void ) 
             {
+                this->_myMap = map_new("salut", 10); 
+                this->_size = 0;
                 std::cout << "Constructor is called." <<std::endl;
                 return ;
             }
@@ -28,15 +32,65 @@ namespace ft
                 return ;
             }
 
+            map & operator=( const map & src ) 
+            {
+                std::cout << "ouiiii\n";
+                this->_myMap = src._myMap;
+                this->_size = src._size;
+                return *this;
+            } 
+
+            T & operator[]( const Key key ) 
+            {
+                _map_node *temp = this->_myMap;
+                T a = 0;
+                for (int i = 0; temp; i++)
+                {
+                    if (key == temp->key)
+                        return (temp->value);  
+                    temp = temp->next;       
+                }
+                
+                this->map_back(&this->_myMap, this->map_new(key, a));
+                return this->_myMap->value;
+            } 
+
+            
 
         private:
-            struct _map_node{
-                Key _key;
-                T   _value;
-            };
+            typedef struct _map_node{
+                Key         key;
+                T           value;
+                _map_node   *next;
+            }               _map_node;
+            _map_node   *_myMap;
+            size_type   _size;
+
+            _map_node  *map_new(Key key, T value)
+            {
+                _map_node *lst = new _map_node;
+
+                lst->key = key;
+                lst->value = value;
+                lst->next = NULL;
+                return lst;
+            }
+
+            void map_back(_map_node **alst, _map_node *other)
+            {
+                _map_node *lst;
+
+                lst = *alst;
+                if (alst == NULL)
+                    *alst = other;
+                else
+                {
+                    while (lst->next)
+                        lst = lst->next;
+                    lst->next = other;
+                }
+            }
     };
 }
 
 #endif //MAP_HPP
-
-//
